829a41f17a0dcc0164848a773b70741f60e78b3e
Abdulaziz-Izzatullaev <myvpn228@gmail.com>
2024-08-05 15:37:27 +0500

    furniture

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -4,40 +4,44 @@ import Delete from "../images/delete";
 import Product from "../components/product";
 
 const page = () => {
-  const getedProducts = JSON.parse(localStorage.getItem("products"));
+  let getedProducts = "";
+  const string = localStorage.getItem("products");
+  if (!!string) {
+    getedProducts = JSON.parse(string);
+  }
   return (
-      <div className=" mb-[100px]">
-        <div className="flex justify-center items-center relative w-full h-[335px]">
-          <img className="w-full absolute  top-[0px]" src="/Main.png" alt="" />
-          <p className=" absolute top-[150px] text-[50px] font-bold">Cart</p>
-        </div>
-        <div>
-          <div className="grid grid-rows-2 w-[917px] h-[200px] gap-y-[62px]">
-            <div className="flex justify-around bg-[#F9F1E7] ">
-              <div className=" h-[55px] flex items-center justify-around">
-                Product
-              </div>
-              <div className=" h-[55px] flex items-center justify-center">
-                Price
-              </div>
-              <div className="h-[55px] flex items-center justify-center">
-                Quantity
-              </div>
-              <div className=" h-[55px] flex items-center justify-around">
-                Subtotal
-              </div>
+    <div className=" mb-[100px]">
+      <div className="flex justify-center items-center relative w-full h-[335px]">
+        <img className="w-full absolute  top-[0px]" src="/Main.png" alt="" />
+        <p className=" absolute top-[150px] text-[50px] font-bold">Cart</p>
+      </div>
+      <div>
+        <div className="grid grid-rows-2 w-[917px] h-[200px] gap-y-[62px]">
+          <div className="flex justify-around bg-[#F9F1E7] ">
+            <div className=" h-[55px] flex items-center justify-around">
+              Product
             </div>
-              {getedProducts?.products.map((item) => (
-                <Product
-                  title={item.title}
-                  id={item._id}
-                  image={item.image}
-                  price={item.price}
-                ></Product>
-              ))}
+            <div className=" h-[55px] flex items-center justify-center">
+              Price
+            </div>
+            <div className="h-[55px] flex items-center justify-center">
+              Quantity
+            </div>
+            <div className=" h-[55px] flex items-center justify-around">
+              Subtotal
+            </div>
           </div>
+          {getedProducts?.products.map((item) => (
+            <Product
+              title={item.title}
+              id={item._id}
+              image={item.image}
+              price={item.price}
+            ></Product>
+          ))}
         </div>
       </div>
+    </div>
   );
 };
 
